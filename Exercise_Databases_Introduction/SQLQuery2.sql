c13402ed455fe19e7bc9a269a8924aa93d770f72
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-05-18 19:16:10 +0300

    Create SQLQuery2.sql

---
+++ Exercise_Databases_Introduction/SQLQuery2.sql
@@ -0,0 +1,11 @@
+-- Select all records from the Towns table
+SELECT * FROM Towns;
+GO
+
+-- Select all records from the Departments table
+SELECT * FROM Departments;
+GO
+
+-- Select all records from the Employees table
+SELECT * FROM Employees;
+GO
