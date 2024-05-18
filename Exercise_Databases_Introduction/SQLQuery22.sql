0b9ffe434349b158461f6f7649e11bc2c6f02cce
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-05-18 19:16:13 +0300

    Create SQLQuery22.sql

---
+++ Exercise_Databases_Introduction/SQLQuery22.sql
@@ -0,0 +1,8 @@
+-- Increase the salary of all employees by 10%
+UPDATE Employees
+SET Salary = Salary * 1.10;
+GO
+
+-- Select only the Salary column for all records in the Employees table
+SELECT Salary FROM Employees;
+GO
