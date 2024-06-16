91ec7af07e44183046dff3f9779312948559e21c
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-06-16 12:50:49 +0300

    Create Solve 7.sql

---
+++ Exam/Solve 7.sql
@@ -0,0 +1,17 @@
+SELECT 
+    l.Name AS Library,
+    c.Email
+FROM 
+    Libraries l
+JOIN 
+    Contacts c ON l.ContactId = c.Id
+WHERE 
+    l.Id NOT IN (
+        SELECT DISTINCT lb.LibraryId
+        FROM LibrariesBooks lb
+        JOIN Books b ON lb.BookId = b.Id
+        JOIN Genres g ON b.GenreId = g.Id
+        WHERE g.Name = 'Mystery'
+    )
+ORDER BY 
+    l.Name ASC;
