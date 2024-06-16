60bd3f588547c4fac61a246fa2f3360f254f520b
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-06-16 12:50:18 +0300

    Create DataBase_Solve 1.sql

---
+++ Exam/DataBase_Solve 1.sql
@@ -0,0 +1,51 @@
+CREATE DATABASE LibraryDb;
+
+CREATE TABLE Contacts (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Email NVARCHAR(100),
+    PhoneNumber NVARCHAR(20),
+    PostAddress NVARCHAR(200),
+    Website NVARCHAR(50)
+);
+
+
+CREATE TABLE Authors (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(100) NOT NULL,
+    ContactId INT NOT NULL,
+    FOREIGN KEY (ContactId) REFERENCES Contacts(Id)
+);
+
+CREATE TABLE Genres (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(30) NOT NULL
+);
+
+
+CREATE TABLE Libraries (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(50) NOT NULL,
+    ContactId INT NOT NULL,
+    FOREIGN KEY (ContactId) REFERENCES Contacts(Id)
+);
+
+
+CREATE TABLE Books (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Title NVARCHAR(100) NOT NULL,
+    YearPublished INT NOT NULL,
+    ISBN NVARCHAR(13) UNIQUE NOT NULL,
+    AuthorId INT NOT NULL,
+    GenreId INT NOT NULL,
+    FOREIGN KEY (AuthorId) REFERENCES Authors(Id),
+    FOREIGN KEY (GenreId) REFERENCES Genres(Id)
+);
+
+
+CREATE TABLE LibrariesBooks (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    LibraryId INT NOT NULL,
+    BookId INT NOT NULL,
+    FOREIGN KEY (LibraryId) REFERENCES Libraries(Id),
+    FOREIGN KEY (BookId) REFERENCES Books(Id)
+);
